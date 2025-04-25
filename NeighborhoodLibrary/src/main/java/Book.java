ca5ab929c7cc5a0ed4dd2e7eb6c53b2540baf1eb
krosengr4 <rosenkev4@gmail.com>
2025-04-25 03:08:01 -0500

    Added constructor for book and getters and setters for elements of Book in Book class

--- NeighborhoodLibrary/src/main/java/Book.java
+++ NeighborhoodLibrary/src/main/java/Book.java
@@ -1,2 +1,61 @@
 public class Book {
+
+    //declare variables
+
+    int id;
+    String isbn;
+    String title;
+    boolean isCheckedOut;
+    String checkedOutTo;
+
+    //Constructor for Book
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo){
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+
+
+    //region Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+    //endregion
+
+
+
 }
