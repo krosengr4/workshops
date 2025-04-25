19eccbfad1ace70e6bf0eed613867dd97b60d777
krosengr4 <rosenkev4@gmail.com>
2025-04-24 20:42:38 -0500

    Created environment to work on present value of ordinary annuity workshop

---
+++ FinancialCalculators/src/main/java/PresentValue.java
@@ -0,0 +1,13 @@
+public class PresentValue {
+
+    public static void main(String[] args) {
+
+// A calculator that determines the present value of an
+//ordinary annuity. (Note: this is difficult)
+//a. It would accept the monthly payout, expected interest rate,
+//and years to pay out from the user
+//b. It would display the present value of that annuity
+
+    }
+
+}
