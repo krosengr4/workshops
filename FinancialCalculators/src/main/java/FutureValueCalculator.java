894bce1a3114ad5abe08780b04f25644caa24760
krosengr4 <rosenkev4@gmail.com>
2025-04-24 20:38:41 -0500

    Completed Future Value Calculator workshop in workshop 1

--- FinancialCalculators/src/main/java/FutureValueCalculator.java
+++ FinancialCalculators/src/main/java/FutureValueCalculator.java
@@ -4,20 +4,29 @@ public class FutureValueCalculator {
 
     public static void main(String[] args) {
 
-        //region Create instance of scanner
-        Scanner myScanner = new Scanner(System.in);
+        //Get user input and store in variables
+        double initialDeposit = getUserInput("What is your initial deposit? ");
+        double interestRate = getUserInput("What is your annual interest rate? ");
+        double numberOfYears = getUserInput("How many years will your deposit earn interest? ");
 
-        //Ask for user info and store in variables
-        System.out.println("What is your initial deposit? ");
-        double initialDeposit = myScanner.nextDouble();
+        //Get interest rate in decimal
+        double decimalIR = interestRate / 100;
 
-        System.out.println("What is your annual interest rate? ");
-        double interestRate = myScanner.nextDouble();
+        //FV = P * (1 +(r / 365))^(365 * t)
+        //Calculate future value
+        double futureValue = initialDeposit * (Math.pow(1 + (decimalIR / 365), (365 * numberOfYears)));
+        //Calculate the amount of interest
+        double totalInterestEarned = futureValue - initialDeposit;
 
-        System.out.println("How many years will your deposit earn interest? ");
-        double numberOfYears = myScanner.nextDouble();
-        //endregion
-
+        //print out future value and interest earned
+        System.out.printf("The Future Value for your CD: $%.2f \nThe total interest earned on your CD is: $%.2f", futureValue, totalInterestEarned);
     }
 
+    //Method that takes a message, shows it to user, and gets user input
+    static double getUserInput(String message) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println(message);
+        String userInput = myScanner.nextLine();
+        return Double.parseDouble(userInput);
+    }
 }
