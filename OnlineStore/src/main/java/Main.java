c80fda2c076739ea3487dc6adb11d2eadb8a6778
krosengr4 <rosenkev4@gmail.com>
2025-05-08 16:45:18 -0500

    added in method to display home screen in Main.java class

--- OnlineStore/src/main/java/Main.java
+++ OnlineStore/src/main/java/Main.java
@@ -1,7 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("\t\t_____WELCOME TO THE ONLINE STORE_____");
+        displayHomeScreen();
+        System.out.println("Thank you for shopping with us! \nWe will see you soon!");
     }
 
+    public static void displayHomeScreen() {
+        boolean ifContinue = true;
+
+        while (ifContinue) {
+            System.out.println("\n\t---HOME PAGE---");
+            //Get number user inputs and return it
+            System.out.println("OPTIONS: \n\t1 - Inventory \n\t2 - Cart \n\t3 - Exit");
+            String userAction = Utils.getUserInput("Enter the number of what you'd like to do: ").trim();
+
+
+            switch (userAction) {
+                case "1" -> Inventory.displayInventoryMenu();
+                case "2" -> Cart.displayCartMenu();
+                case "3" -> ifContinue = false;
+                default -> System.err.println("ERROR: Please choose a number between 1 and 3!");
+            }
+
+        }
+    }
 }
