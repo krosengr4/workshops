3f012e47cd7976a136c0f29ef6baaa37ea44e68f
krosengr4 <rosenkev4@gmail.com>
2025-05-08 16:42:57 -0500

    Added utils to get user input and pause app in Utils.java class

--- OnlineStore/src/main/java/Utils.java
+++ OnlineStore/src/main/java/Utils.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Utils {
+
+    public static String filePath = "Online-Store/src/main/resources/products.csv";
+
+    static Scanner myScanner = new Scanner(System.in);
+
+    static String getUserInput(String message) {
+        System.out.println(message);
+        return myScanner.nextLine();
+    }
+
+    public static void pauseApp() {
+        System.out.println("\n Please press enter to continue...");
+        myScanner.nextLine();
+    }
+
 }
