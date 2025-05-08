42001ea211c48049f4941f354f4fc2f7c331eb25
krosengr4 <rosenkev4@gmail.com>
2025-05-08 16:43:55 -0500

    Added in Product constructor with getters and setters in Product.java class

--- OnlineStore/src/main/java/Product.java
+++ OnlineStore/src/main/java/Product.java
@@ -1,2 +1,45 @@
 public class Product {
+
+    String id;
+    String name;
+    double price;
+    String department;
+
+    // to create new instances of Product and set values
+    public Product(String id, String name, double price, String department){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    //region Getters and Setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    //endregion
 }
