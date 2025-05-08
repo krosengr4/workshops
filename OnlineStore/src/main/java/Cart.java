803afe1ac257d3d5baffa1678a6696ca6f4fe83a
krosengr4 <rosenkev4@gmail.com>
2025-05-08 16:50:30 -0500

    Created methods in Cart.java class

--- OnlineStore/src/main/java/Cart.java
+++ OnlineStore/src/main/java/Cart.java
@@ -1,2 +1,81 @@
+import java.util.ArrayList;
+
 public class Cart {
+
+    public static void displayCartMenu() {
+        boolean ifContinue = true;
+
+        while (ifContinue) {
+            System.out.println("\n\t\t---YOUR CART---");
+            System.out.println("OPTIONS: \n\t1 - Display your cart \n\t2 - Check Out \n\t3 - Remove a product from the cart \n\t4 - Return to Home Page");
+            String userAction = Utils.getUserInput("Enter the number of what you'd like to do: ").trim();
+
+            switch (userAction) {
+                case "1" -> displayCart();
+                case "2" -> checkOut();
+                case "3" -> removeItem();
+                case "4" -> ifContinue = false;
+                default -> System.out.println("ERROR! Please Enter a Number Between 1-4");
+            }
+        }
+    }
+
+    public static void displayCart() {
+        System.out.println("\t---YOUR CART---");
+
+        ArrayList<Product> userCart = Inventory.userCart;
+
+        if (userCart.isEmpty()) {
+            System.out.println("There are no items in your cart. \nYou can add items to your cart from inventory screen!");
+        } else {
+            double totalPrice = 0;
+            for (Product p : userCart) {
+                System.out.println(p.getName() + " $" + p.getPrice());
+                totalPrice += p.getPrice();
+            }
+            System.out.printf("\nTotal Amount: $%.2f", totalPrice);
+        }
+        Utils.pauseApp();
+    }
+
+    public static void checkOut() {
+        displayCart();
+
+        ArrayList<Product> userCart = Inventory.userCart;
+
+        System.out.println("\t---CHECKOUT---");
+
+        System.out.println("OPTIONS:\n\tC - Credit \n\tD - Debit");
+        String userPayment = Utils.getUserInput("How would you like to pay: ").trim();
+
+        if (userPayment.equalsIgnoreCase("c") || userPayment.equalsIgnoreCase("d")) {
+            String userCCNumber = Utils.getUserInput("Enter your card number: ");
+            String userCCExpiration = Utils.getUserInput("Enter the expiration date: ");
+            String userCCCVV = Utils.getUserInput("Enter the 3 digit CVV: ");
+        } else {
+            System.err.println("Error! Please enter either c or d.");
+        }
+        System.out.println("Thank you for your purchase! You will be sent an email with details of your purchase!");
+        userCart.clear();
+        Utils.pauseApp();
+    }
+
+    public static void removeItem() {
+        ArrayList<Product> userCart = Inventory.userCart;
+
+        if (userCart.isEmpty()) {
+            System.out.println("There are no items in your cart. \nYou can add items to your cart from inventory screen!");
+        } else {
+            System.out.println("\t---ITEMS IN YOUR CART---");
+            for (Product p : userCart) {
+                System.out.println(p.getName());
+            }
+            String userRemove = Utils.getUserInput("\nEnter the name of the item you would like to remove: ").trim();
+
+            userCart.removeIf(p -> userRemove.equalsIgnoreCase(p.getName()));
+            System.out.println(userRemove + "has been removed.");
+            Utils.pauseApp();
+        }
+
+    }
 }
